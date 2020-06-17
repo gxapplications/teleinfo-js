f92755d0e5c94e996c834968f5669de7d7f35a35
Xavier Gouley <xavier.gouley@gmail.com>
2020-06-18 01:54:30 +0200

    third commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TODO
 
-:copyright: 2020 GXApplications. | [License](https://github.com/gxapplications/teleinfo-js/blob/master/LICENSE) ]
+:copyright: 2020 GXApplications. | [License](https://github.com/gxapplications/teleinfo-js/blob/master/LICENSE)
--- teleinfo.js
+++ teleinfo.js
@@ -1,29 +1,35 @@
 /* https://www.enedis.fr/sites/default/files/Enedis-NOI-CPT_54E.pdf */
 
-const events = require('events');
-const serialport = require('serialport');
-const Readline = require('@serialport/parser-readline');
-const isEqual = require('lodash.isequal');
-const reduce = require('lodash.reduce');
+const events = require('events')
+const Serialport = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const isEqual = require('lodash.isequal')
+const reduce = require('lodash.reduce')
 
 function compareObjects (a, b) {
   const diffs = reduce(a, function (acc, v, k) {
-    return isEqual(v, b[k]) ? acc : acc.k = { old: v, new: b[k] };
-  }, {});
+    if (!isEqual(v, b[k])) {
+      acc.k = { old: v, new: b[k] }
+    }
+    return acc
+  }, {})
   return reduce(b, function (acc, v, k) {
-    return isEqual(v, a[k]) ? acc : acc.k = { old: a[k], new: v };
-  }, diffs);
+    if (!isEqual(v, a[k])) {
+      acc.k = { old: a[k], new: v }
+    }
+    return acc
+  }, diffs)
 }
 
 function parseRaw (data, frame, frameEvents) {
-  const elements = data.split(' ');
+  const elements = data.split(' ')
   if (elements.length === 3) {
-    let sum = 0;
-    let j;
+    let sum = 0
+    let j
     for (j = 0; j < data.length - 2; j++) {
-      sum += data.charCodeAt(j);
+      sum += data.charCodeAt(j)
     }
-    sum = (sum & 63) + 32;
+    sum = (sum & 63) + 32
     if (sum === data.charCodeAt(j + 1)) {
       switch (elements[0].substr(0, 4)) {
         case 'BASE': // Index Tarif bleu
@@ -37,69 +43,70 @@ function parseRaw (data, frame, frameEvents) {
         case 'IMAX': // Intensité max appelée (1/2/3 pour triphasé)
         case 'PAPP': // Puissance apparente
         case 'PMAX': // Puissance max triphasée atteinte
-          frame[elements[0]] = Number(elements[1]);
-          break;
+          frame[elements[0]] = Number(elements[1])
+          break
         default:
-          frame[elements[0]] = elements[1];
+          frame[elements[0]] = elements[1]
       }
-      return true;
+      return true
     } else {
-      const error = new Error('Checksum error: \n' + data + '\nComputed/Receive: ' + sum + '/' + data.charCodeAt(j+1));
-      frameEvents.emit('error', error);
+      const error = new Error('Checksum error: \n' + data + '\nComputed/Receive: ' + sum + '/' + data.charCodeAt(j + 1))
+      frameEvents.emit('error', error)
     }
   } else {
     // TODO :0! what to do ? error ?
   }
-  return false;
+  return false
 }
 
 function teleinfo (port) {
-  const frameEvents = new events.EventEmitter();
-  let isFirstFrame = true;
-  let lastFrame = {};
-  const serialPort = new serialport(
+  const frameEvents = new events.EventEmitter()
+  let isFirstFrame = true
+  let lastFrame = {}
+  const serialPort = new Serialport(
     port,
     { baudRate: 1200, dataBits: 7, parity: 'even', stopBits: 1 }
-  );
-  const parser = new Readline({ delimiter: String.fromCharCode(13, 3, 2, 10) });
-  serialPort.pipe(parser);
+  )
+  const parser = new Readline({ delimiter: String.fromCharCode(13, 3, 2, 10) })
+  serialPort.pipe(parser)
 
   parser.on('data', function (data) {
-    frameEvents.emit('rawFrame', data);
-    isFirstFrame = false;
-  });
+    frameEvents.emit('rawFrame', data)
+    isFirstFrame = false
+  })
 
   parser.on('error', function (err) {
-    frameEvents.emit('error', err);
-  });
+    frameEvents.emit('error', err)
+  })
 
   frameEvents.on('rawFrame', function (data) {
-    const frame = {};
+    const frame = {}
     data.split('\r\n').forEach((d) => {
-      parseRaw(d, frame, frameEvents);
-    });
+      parseRaw(d, frame, frameEvents)
+    })
 
     // test if ADCO is present (first line of the frame)
     if (frame.ADCO !== undefined) {
-      frameEvents.emit('frame', frame);
+      frameEvents.emit('frame', frame)
     } else if (!isFirstFrame) { // avoid error to be thrown if first frame is not complete (just ignore it)
-      frameEvents.emit('error', new Error('Partial frame'));
+      frameEvents.emit('error', new Error('Partial frame'))
     }
-  });
+  })
 
   frameEvents.on('frame', function (data) {
-    const diff = compareObjects(lastFrame, data);
+    const diff = compareObjects(lastFrame, data)
     if (Object.keys(diff).length > 0) {
-      const changes = {};
-      for (let attr in diff) {
-        changes[attr] = diff[attr].new;
+      const changes = {}
+      for (const attr in diff) {
+        changes[attr] = diff[attr].new
       }
-      frameEvents.emit('change', { changes: changes, t: Date.now() });
-      frameEvents.emit('diff', { old: lastFrame, new: data, diff: diff, t: Date.now() });
+      frameEvents.emit('change', { changes: changes, t: Date.now() })
+      frameEvents.emit('diff', { old: lastFrame, new: data, diff: diff, t: Date.now() })
     }
-  });
+    lastFrame = data
+  })
 
-  return frameEvents;
+  return frameEvents
 }
 
-module.exports = teleinfo;
+module.exports = teleinfo
